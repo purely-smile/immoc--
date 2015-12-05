13cde705e3182ae01ce779955146080cf7bafb14
purely-smile <wang1234561211@outlook.com>
2015-12-05 17:01:17 +0800

    增加学习笔记
    把之前记录知识的笔记以后同步到git上，今天第一次使用git，发现还挺好用的，以后都用git管理以后联系的代码和笔记，同时也是个记录成长的过程。

--- 网页定位导航特效/8-1编程挑战/test.js
+++ 网页定位导航特效/8-1编程挑战/test.js
@@ -1,20 +1,19 @@
-$(function(){
-	$(window).scroll(function(){
-		var menus=$("#menu");
-		var top=$(document).scrollTop();
-		var items=$(".item");
-		var currentId="";
-		items.each(function(){
+$(function(){                            //页面载入触发
+	$(window).scroll(function(){         //滚动页面触发
+		var menus=$("#menu");                //保存id为menu的标签，这里为导航div
+		var top=$(document).scrollTop();     //保存滚动条top值
+		var items=$(".item");               //保存class为item的元素数组
+		var currentId="";                  //定义临时变量
+		items.each(function(){               //遍历数组
 			var that=$(this);
-			if(top>that.offset().top-100){
-				currentId="#"+that.attr("id");
+			if(top>that.offset().top-100){    //找到对应的div
+				currentId="#"+that.attr("id");    //获取到的id为item1，前面加#，方便后面使用
 			}else{
-				return false;
+				return false;            //反之，跳出循环
 			}
 		})
 		var currentLink=menus.find(".current");   //find class值为current的a标签
-				//console.log(currentLink)   
-			if(currentId!=currentLink.attr("href")&&currentId){
+			if(currentId!=currentLink.attr("href")&&currentId){     //如果currentId存在，同时不是当前激活的div，就给增加current属性，同时移除之前的属性。
 				currentLink.removeClass("current");
 				menus.find("[href="+currentId+"]").addClass("current");
 			}			
